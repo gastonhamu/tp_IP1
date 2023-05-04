05d64434c3d10b69dac53fd4d3c125f271ea837e
Gaston Hamu <gastonhamu@gmail.com>
2023-05-04 19:26:37 -0300

    Subiendo el tp

---
+++ iap1-tp.hs
@@ -0,0 +1,76 @@
+-- Completar con los datos del grupo
+--
+-- Nombre de Grupo: Maquinas
+-- Integrante 1: Gastón Julián Hamu, gastonhamu@gmail.com, 1149/22
+-- Integrante 2: Tatiana Grinspan, tatugrin@gmail.com, 350/22
+-- Integrante 3: Santiago Riso, santiagoriso@gmail.com , 792/22
+-- Integrante 4: Dante Waisman, waismandante@gmail.com, 1163/22
+
+type Usuario = (Integer, String) -- (id, nombre)
+type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
+type Publicacion = (Usuario, String, [Usuario]) -- (usuario que publica, texto publicacion, likes)
+type RedSocial = ([Usuario], [Relacion], [Publicacion])
+
+-- Funciones basicas
+
+usuarios :: RedSocial -> [Usuario]
+usuarios (us, _, _) = us
+
+relaciones :: RedSocial -> [Relacion]
+relaciones (_, rs, _) = rs
+
+publicaciones :: RedSocial -> [Publicacion]
+publicaciones (_, _, ps) = ps
+
+idDeUsuario :: Usuario -> Integer
+idDeUsuario (id, _) = id 
+
+nombreDeUsuario :: Usuario -> String
+nombreDeUsuario (_, nombre) = nombre 
+
+usuarioDePublicacion :: Publicacion -> Usuario
+usuarioDePublicacion (u, _, _) = u
+
+likesDePublicacion :: Publicacion -> [Usuario]
+likesDePublicacion (_, _, us) = us
+
+-- Ejercicios
+
+nombresDeUsuarios :: RedSocial -> [String]
+nombresDeUsuarios = undefined
+
+-- describir qué hace la función: .....
+amigosDe :: RedSocial -> Usuario -> [Usuario]
+amigosDe = undefined
+
+-- describir qué hace la función: .....
+cantidadDeAmigos :: RedSocial -> Usuario -> Int
+cantidadDeAmigos = undefined
+
+-- describir qué hace la función: .....
+usuarioConMasAmigos :: RedSocial -> Usuario
+usuarioConMasAmigos = undefined
+
+-- describir qué hace la función: .....
+estaRobertoCarlos :: RedSocial -> Bool
+estaRobertoCarlos = undefined
+
+-- describir qué hace la función: .....
+publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
+publicacionesDe = undefined
+
+-- describir qué hace la función: .....
+publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
+publicacionesQueLeGustanA = undefined
+
+-- describir qué hace la función: .....
+lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
+lesGustanLasMismasPublicaciones = undefined
+
+-- describir qué hace la función: .....
+tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
+tieneUnSeguidorFiel = undefined
+
+-- describir qué hace la función: .....
+existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
+existeSecuenciaDeAmigos = undefined
